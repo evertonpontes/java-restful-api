6f992c3c196fe002a7e98f6337d0468482f0cf54
Everton <evertonpontesp@gmail.com>
2024-07-27 10:55:50 -0300

    first commit

---
+++ src/main/java/me/dio/api/SantanderBackendJava2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBackendJava2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBackendJava2024Application.class, args);
+	}
+
+}
