d1c13ff7b99e26f7b5945812331d62922f6eeff5
Everton <evertonpontesp@gmail.com>
2024-07-27 12:16:01 -0300

    Implementação da camada de domínio.

---
+++ src/main/java/me/dio/api/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package me.dio.api.domain.repository;
+
+import me.dio.api.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
