3735bf43d7eb834782bfaede051802126be72be5
Everton <evertonpontesp@gmail.com>
2024-07-28 10:45:25 -0300

    Implementação do controller.

--- src/main/java/me/dio/api/domain/repository/UserRepository.java
+++ src/main/java/me/dio/api/domain/repository/UserRepository.java
@@ -4,4 +4,6 @@ import me.dio.api.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
 }
---
+++ src/main/java/me/dio/api/service/UserService.java
@@ -0,0 +1,10 @@
+package me.dio.api.service;
+
+import me.dio.api.domain.model.User;
+
+public interface UserService {
+
+    User findById(Long id);
+
+    User create(User userToCreate);
+}
